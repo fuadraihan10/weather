a32ec2109c45ca1911b17f57ddbe814900cce87d
FuadRaihan <fuadraihan100@gmail.com>
2026-05-29 10:55:28 +0600

    made a new hooked callled useWeatherData which fetches data from openweather api and return data,error,loadingstate

--- weather_dashboard/src/components/Weather/WeatherBoard.jsx
+++ weather_dashboard/src/components/Weather/WeatherBoard.jsx
@@ -1,8 +1,11 @@
 import AddtoFevourite from "./AddtoFevourite";
 import Weatherdata from "./Weatherdata";
 import Weatherheadline from "./weatherheadline";
+import {useWeather} from "../../hooks/index.js"
 
 export default function WeatherBoard() {
+        const{weatherData,error,isLoading} = useWeather()
+        console.log(weatherData,error,isLoading);
     return (
         <div className="container">
             <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/14 px-4 lg:px-14 py-6 lg:py-10 min-h-130 max-w-264.5 mx-auto">
---
+++ weather_dashboard/src/hooks/useWeather.js
@@ -0,0 +1,89 @@
+import { useCallback, useEffect, useState } from "react";
+
+//main function to fetch weather data---------------------------
+export default function useWeather() {
+    //weatherdata template------------
+    const [weatherData, setWeatherData] = useState({
+        location: "",
+        climate: "",
+        temperature: "",
+        maxTemperature: "",
+        minTemperature: "",
+        humidity: "",
+        cloudPercentage: "",
+        wind: "",
+        time: "",
+        longitude: "",
+        latitude: "",
+    });
+    const [isLoading, setIsLoading] = useState({ state: false, message: "" });
+    const [error, setError] = useState(null);
+
+    //fetching weather data function according to latitude and longitude------------------------------------------------------
+    const fetchWeatherData = useCallback(async (latitude, longitude) => {
+        //main code to fetch weather data from openweathermap API----------------------
+        try {
+            setIsLoading({ state: true, message: "Fetching weather data..." });
+
+            //fetching weather data ----------------------
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`,
+            );
+
+            //if response causes any error-------------
+            if (!response.ok) {
+                throw new Error(
+                    `Fetching weather data failed: ${response.status}`,
+                );
+            }
+
+            const data = await response.json();
+
+            //adding actual weather data to the weatherdata state-----------------
+            setWeatherData({
+                location: data?.name,
+                climate: data?.weather[0]?.main,
+                temperature: data?.main?.temp,
+                maxTemperature: data?.main?.temp_max,
+                minTemperature: data?.main?.temp_min,
+                humidity: data?.main?.humidity,
+                cloudPercentage: data?.clouds?.all,
+                wind: data?.wind?.speed,
+                time: data?.dt,
+                longitude,
+                latitude,
+            });
+        } catch (err) {
+            //error handling---------------------------
+            setError(err);
+        } finally {
+            // after fetching data-----------------
+            setIsLoading({ state: false, message: "" });
+        }
+    }, []);
+    //-----------------------------------------------------------------------------------------------------------------
+
+    useEffect(() => {
+        setIsLoading({ state: true, message: "Finding location..." });
+
+        //collectiong users latitude and longitude-------------
+        navigator.geolocation.getCurrentPosition(
+            //if location found------------
+            (position) => {
+                //calling the main function (async)-----------------
+                fetchWeatherData(
+                    position.coords.latitude,
+                    position.coords.longitude,
+                );
+            },
+            //if location is not found ------------------------
+            (err) => {
+                setError(new Error("Location access denied: " + err.message));
+                setIsLoading({ state: false, message: "" });
+            },
+        );
+    }, [fetchWeatherData]);
+
+    //return the states
+    return { weatherData, error, isLoading };
+}
